fc8571bd8a59f46aa750e9c8eec9c37f0c723f2d
Thezone-1 <somoprovobhattacharjee@gmail.com>
2023-05-31 21:19:29 +0530

    Added the CodeBase
    The body and functinalities of the banking system

---
+++ BankingProgram.java
@@ -0,0 +1,50 @@
+package java_programs.gg_bank;
+
+import java.util.Scanner;
+
+public class BankingProgram {
+
+    public static void main(String[] args){
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        String accountNumber = scanner.nextLine();
+
+        System.out.print("Enter initial balance: ");
+        double initialBalance = scanner.nextDouble();
+
+        BankAccount account = new BankAccount(accountNumber, initialBalance);
+
+        while (true) {
+            System.out.println("\nSelect an option:");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    account.checkBalance();
+                    break;
+                case 4:
+                    System.out.println("Exiting program...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+}
