bc2fdb73aed44c9102643b486b0bf6fc4f693fb6
Manosrija Nasrin <manosrijanasrin@gmail.com>
2023-06-01 23:43:28 +0530

    Incorporated PassbookEntry class functionality

--- src/main/java/banking_management/BankAccount.java
+++ src/main/java/banking_management/BankAccount.java
@@ -1,31 +1,39 @@
 package banking_management;
 
+import java.util.Date;
+
 public class BankAccount {
     private String accountNumber;
-    private double balance;
-    BalanceControl balanceControl;
+    private Double balance;
+    private BalanceControl balanceControl;
+    private PassbookEntry passbookEntry;
 
     public BankAccount(String accountNumber) {
         this.accountNumber = accountNumber;
-        balanceControl = new BalanceControl();
+        this.balanceControl = new BalanceControl();
         this.balance = balanceControl.getBalance();  /* read from file */
+        this.passbookEntry = new PassbookEntry();
     }
 
-    public void deposit(double amount) {
+    public void deposit(Double amount) {
+        Date timestamp = new Date();
         balance += amount;
         System.out.printf("%s: Deposit successful. New balance: Rs. %.2f\n", accountNumber, balance);
         balanceControl.setBalance(balance);
+        passbookEntry.createTransactionEntry(timestamp, "Credit", accountNumber, amount, balance);
         balanceControl.commitBalance();
     }
 
-    public void withdraw(double amount) {
+    public void withdraw(Double amount) throws IllegalStateException {
+        Date timestamp = new Date();
         if (amount > balance) {
-            System.out.println(accountNumber + "Insufficient funds. Withdrawal failed.");
+            throw new IllegalStateException(accountNumber + ": Insufficient funds. Withdrawal failed.");
         } else {
             balance -= amount;
             System.out.printf("%s: Withdrawal successful. New balance: Rs. %.2f\n", accountNumber, balance);
         }
         balanceControl.setBalance(balance);
+        passbookEntry.createTransactionEntry(timestamp, "Debit", accountNumber, amount, balance);
         balanceControl.commitBalance();
     }
 
